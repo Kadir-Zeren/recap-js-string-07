0508f55bd46a2c6ce2a125387acd8ce24f0de40f
kadir-zeren <k4dzeren@gmail.com>
2024-03-20 15:50:00 +0300

    for string

---
+++ app.js
@@ -0,0 +1,25 @@
+// let str1 = "this is a string";
+// let str2 = "this is a string";
+// let str3 = `this is a string`;
+// let str4 = new String("This is a string");
+
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(str3);
+// console.log(str4);
+
+// console.log(str3[0]);
+
+// const str = "Hello World";
+
+// console.log(str.length);
+// console.log(str[6]);
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   console.log(str[i]);
+// }
+
